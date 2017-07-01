3f43efc00b3026d006040c2d75e59766da9edfc2
Pierre-Louis Chevallier <pierrelouis.chevallier60@gmail.com>
2017-07-01 17:21:06 +0200

    first commit

---
+++ theory/insertion_sort.py
@@ -0,0 +1,13 @@
+import pdb
+
+arr = [5, 2, 4, 6, 1, 3]
+
+for j in range(1, len(arr)):
+	key = arr[j]
+	i = j-1
+	while i >= 0 and arr[i] > key:
+		arr[i+1] = arr[i]
+		i = i-1
+	arr[i+1] = key
+
+print(arr)
